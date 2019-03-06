bbc2b3d3ff37881c1e84e07aa7db8dc71bcec4e9
Crystal <48270378+Crystal-JJ@users.noreply.github.com>
2019-03-06 13:48:57 +0800

    Update result.r

--- source/result.r
+++ source/result.r
@@ -6,14 +6,15 @@
  e <- read.table("genez-order.txt",header=F)
  f <- read.table("net-name.txt",header=F) 
 
- a1 <- 0.2*a
+ a1 <- 0.3*a
  b1 <- 0.9*b
- c1 <- 0.3*c
- d1 <- 0.3*d
- e1 <- 0.4*e
+ c1 <- 0.5*c
+ d1 <- 0.4*d
+ e1 <- 0.2*e
 
  x <- a1+b1+c1+d1+e1
 
  y <- cbind(f,x)
 
- write.table(y,"result29334.txt",sep="\t",col.names=F,row.names=F,quote=F)
+ write.table(y,"result39542.txt",sep="\t",col.names=F,row.names=F,quote=F)
+
